a41cde1c50f3f912705d1b898ef884374af1ffec
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-10-27 12:30:42 -0500

    set up the project, added connection string to the configuration file

---
+++ ChinookAPI_Dapper/Controllers/ChinookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChinookAPI_Dapper.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ChinookAPI_Dapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChinookController : ControllerBase
+    {
+        private readonly Chinook _chinook;
+
+        public ChinookController(IConfiguration config)
+        {
+            _chinook = new Chinook(config);
+        }
+    }
+}
